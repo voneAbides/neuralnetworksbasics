458c0c9a16da8326f8ad794dbee4cb169f1f478c
vone <vojtiknemecek@seznam.cz>
2020-12-06 18:30:10 +0100

    y understood

--- SDPTA.py
+++ SDPTA.py
@@ -3,15 +3,16 @@ import numpy as np
 
 from pip._vendor.msgpack.fallback import xrange
 
+P = 35000
 c = 0.1
-W = [0.1 * (uniform(-1, 2)) for _ in xrange(3)]  # 3 random numbers in range (-1,2) - initial weights
+W = np.array([0.1 * (uniform(-1, 2)) for _ in xrange(3)]).T  # 3 random numbers in range (-1,2) - initial weights
 E = 0
 p = 1
 k = 1
-
+y = np.array([1, 1, 1])
 print(W)
 
-# while True:
-#     while p <= p:
-#         o = np.sign(W * y).
+while True:
+    while p <= p:
+        o = np.sign(np.dot(W, y))
 
