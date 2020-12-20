d057507ef3461cb61a4f2cb1ba26ef6adea0f3a7
voneAbides <44479267+voneAbides@users.noreply.github.com>
2020-12-20 21:35:29 +0100

    Added other algorithms files

--- SDPTA.py
+++ SDPTA.py
@@ -3,23 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-P = 35000
-x1 = np.random.rand(1, 5)
-d1 = np.random.rand(1, 5)
+x1 = np.random.rand(1, 1)
+y1 = np.random.rand(1, 1)
 
-x2 = np.random.rand(1, 5)
-d2 = np.random.rand(1, 5)
+x2 = np.random.rand(1, 1)
+y2 = np.random.rand(1, 1)
 
-D = [[x1, d1], [x2, d2]]  #Dataset indexted in p
-print(D)
+x3 = np.random.rand(1, 1)
+y3 = np.random.rand(1, 1)
+
+x4 = np.random.rand(1, 1)
+y4 = np.random.rand(1, 1)
+
+y = np.array([[x1, y1, 1],
+            [x2, y2, 1],
+            [x3, y3, 1],
+            [x4, y4, 1]])   #Dataset indexted in p
 
 
-def sdpta(D):
-    P = len(D)
-    dim = len(D[0][0])  #x1 is same len as x2
+d = [0, 1, 1, 0]
+print(y)
+
+
+def sdpta(y):
+    P = len(y)
+    dim = len(y[0][0])  #x1 is same len as x2
 
     c = 0.1
-    w = 0.1 * (-1 + 2 * np.random.rand(3,1))  # 3 random numbers in range (-1,2) - initial weights [0.1 * (uniform(-1, 2)) for _ in xrange(3)] np.array(0.1 * (np.random.rand(dim, 1))).T
+    w = np.array(0.1 * (-1 + 2 * np.random.rand(3, 1)))  # 3 random numbers in range (-1,2) - initial weights [0.1 * (uniform(-1, 2)) for _ in xrange(3)] np.array(0.1 * (np.random.rand(dim, 1))).T
     print('Weights:\n' + str(w))
 
     e = 0
@@ -27,24 +38,30 @@ def sdpta(D):
     k = 1
 
     while True:
-        while p <= P:
-            x, d = D[p]
-            o = np.sign(np.dot(w.T, x))
-            w = w + np.dot(0.5 * c * (d - o), x)
-            e = 0.5 * (d - o)**2 + e
+        while p < P:
+            # x, d = D[p]
+            o = np.sign(np.dot(w.T, y[:, p]))
+            w = w + np.dot(0.5 * c * (d[p] - o), y[:, p])
+            e = 0.5 * (d[p] - o)**2 + e
             p += 1
 
-            x1 = [0, 1]
+            x0 = [0, 1]
 
             if w[2] == 0:
                 w[2] = w[2] + 0.0001
 
-            y1 = (-w[3] / w[2]) - ((w[1] * x1 ) / w[2])
+            y0 = (-w[3] / w[2]) - ((w[1] * x0 ) / w[2])
 
-            if e == 0:
-                break
+        print(e)
+
+        if e == 0:
+            break
+        else:
+            e = 0
+            p = 0
 
         k = k + 1
 
 
-sdpta(D)
+
+sdpta(y)
